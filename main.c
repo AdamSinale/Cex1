12e74f334413cdcc54eb1ba6c8de2926cf8802de
AdamSinale <121156729+AdamSinale@users.noreply.github.com>
2024-01-22 10:00:13 +0200

    Update main.c cuz matan is king

--- main.c
+++ main.c
@@ -15,19 +15,19 @@ int main(){
     }
 
     printf("The Armstrong numbers are:");
-    for(int i=num1; i<num2; i++){
+    for(int i=num1; i<=num2; i++){
         if(isArmstrong(i) == 1){ printf(" %d", i); }
     }
     printf("\nThe Palindromes numbers are:");
-    for(int i=num1; i<num2; i++){
+    for(int i=num1; i<=num2; i++){
         if(isPalindrome(i) == 1){ printf(" %d", i); }
     }
     printf("\nThe Prime numbers are:");
-    for(int i=num1; i<num2; i++){
+    for(int i=num1; i<=num2; i++){
         if(isPrime(i) == 1){ printf(" %d", i); }
     }
     printf("\nThe Strong numbers are:");
-    for(int i=num1; i<num2; i++){
+    for(int i=num1; i<=num2; i++){
         if(isStrong(i) == 1){ printf(" %d", i); }
     }
     printf("\n");
