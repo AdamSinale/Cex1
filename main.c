f0a959abf0287080d78b101dc877b541ea6acf88
AdamSinale <121156729+AdamSinale@users.noreply.github.com>
2024-01-24 17:00:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(){
     for(int i=num1; i<=num2; i++){
         if(isArmstrong(i) == 1){ printf(" %d", i); }
     }
-    printf("\nThe Palindromes numbers are:");
+    printf("\nThe Palindromes are:");
     for(int i=num1; i<=num2; i++){
         if(isPalindrome(i) == 1){ printf(" %d", i); }
     }
