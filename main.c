e594176f415b0845027033f62b594fd3ddc2fc88
AdamSinale <121156729+AdamSinale@users.noreply.github.com>
2024-01-24 17:09:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -18,18 +18,18 @@ int main(){
     for(int i=num1; i<=num2; i++){
         if(isArmstrong(i) == 1){ printf(" %d", i); }
     }
-    printf("\nThe Palindromes are:");
+    printf("\n\rThe Palindromes are:");
     for(int i=num1; i<=num2; i++){
         if(isPalindrome(i) == 1){ printf(" %d", i); }
     }
-    printf("\nThe Prime numbers are:");
+    printf("\n\rThe Prime numbers are:");
     for(int i=num1; i<=num2; i++){
         if(isPrime(i) == 1){ printf(" %d", i); }
     }
-    printf("\nThe Strong numbers are:");
+    printf("\n\rThe Strong numbers are:");
     for(int i=num1; i<=num2; i++){
         if(isStrong(i) == 1){ printf(" %d", i); }
     }
-    printf("\n");
+    printf("\n\r");
     return 0;
 }
